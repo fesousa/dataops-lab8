b0dea19286a31db1e991006116b3dfb0f5dfbcbb
Fernando Sousa <fernandosousa.ti@gmail.com>
2021-10-24 14:44:39 -0300

    atualizacao lambda

--- app/charts.py
+++ app/charts.py
@@ -24,7 +24,9 @@ def handler(event, context):
     except Exception as e:
         raise
 
-    return {'statusCode': 200, "body":json.dumps(res)}
+    return {'statusCode': 200, "body":json.dumps(res), "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"}}
 
 def execute_sql(client, sql_text, redshift_database, redshift_user, redshift_cluster_id, with_event=True):
     print("Executing: {}".format(sql_text))
